f344aaaf8cc8ac2238653c3d04b59005980aead1
Zhenyu Zheng <zheng.zhenyu@outlook.com>
2020-03-20 10:59:25 +0800

    Add arm_cap_support
    Add arm_cap_support

--- src/cpu/cpu_arm.c
+++ src/cpu/cpu_arm.c
@@ -46,6 +46,13 @@ static const char *lsCpuPath = "/usr/bin/lscpu";
 #define LSCPU lsCpuPath
 #define MAX_LSCPU_SIZE = (1024*1024)   /* 1MB limit for lscpu output */
 
+VIR_LOG_INIT("cpu.cpu_arm");
+
+static const char *sysinfoCpuinfo = "/proc/cpuinfo";
+
+#define CPUINFO sysinfoCpuinfo
+#define CPUINFO_FILE_LEN (1024*1024)   /* 1MB limit for /proc/cpuinfo file */
+
 static const virArch archs[] = {
     VIR_ARCH_ARMV6L,
     VIR_ARCH_ARMV7B,
